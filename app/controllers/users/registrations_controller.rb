bc3692bfc26dc2dcfade293196bcdb885ae21149
komeda <rikomatsu0313@gmail.com>
2025-09-02 07:34:06 +0900

    lint修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -7,8 +7,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def update_resource(resource, params)
-    return super if params['password'].present?
+    return super if params["password"].present?
 
-    resource.update_without_password(params.except('current_password'))
+    resource.update_without_password(params.except("current_password"))
   end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -269,7 +269,7 @@ Devise.setup do |config|
   config.sign_out_via = :delete
 
   # ==> OmniAuth
-    config.omniauth :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
+  config.omniauth :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
