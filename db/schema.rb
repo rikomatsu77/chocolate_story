5d52ce2a4d4b6c44d1541a8076a0019d0ee38231
komeda <rikomatsu0313@gmail.com>
2025-09-02 07:19:33 +0900

    Google認証

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_08_08_193632) do
+ActiveRecord::Schema[7.2].define(version: 2025_09_01_054715) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,7 +28,10 @@ ActiveRecord::Schema[7.2].define(version: 2025_08_08_193632) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 end
